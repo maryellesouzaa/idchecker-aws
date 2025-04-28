65361476da27e390ed86a89dfd412d2e6023d86f
Maryelle Souza <mary2011hta@hotmail.com>
2025-04-28 19:45:40 -0300

    sera

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from datetime import datetime
 import psycopg2
 import re
 
